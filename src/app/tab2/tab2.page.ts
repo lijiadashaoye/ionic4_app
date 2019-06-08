945c7b95bb243297fc287cc1eb4f64b3ed0115f8
lipeng <lipengd@litsoft.com.cn>
2019-06-08 17:11:02 +0800

    f

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,9 +9,16 @@ import {
 })
 export class Tab2Page {
   left_lists = []; // 左侧导航
+  right_lists = []; // 右侧产品展示数据
   ngOnInit(): void {
     for (let i = 1; i < 20; i++) {
       this.left_lists.push(`分类${i}`)
+    }
+    for (let i = 0; i < 12; i++) {
+      this.right_lists.push({
+        pic: `assets/images/list${i+1}.jpg`,
+        title: `第${i+1}个`
+      })
     }
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  productList = [];
+
+  ngOnInit(): void {
+    for (let i = 3; i--;) {
+
+      let obj = {
+        bottom: {
+          img: `assets/images/list${i+1}.jpg`,
+          title: '产品介绍',
+          checked: '已选产品',
+          zheng: 5,
+          ling: 30,
+        }
+      }
+      if (i % 2 === 0) {
+        obj['top'] = {
+          title: '可以换购',
+          type: '去换购'
+        }
+      }
+      this.productList.push(obj)
+    }
+  }
+}
